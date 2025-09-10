e055cccf6e63c2c496cf1a4fd8ad6e4089255c9e
Keane Small <48247528+Keane-Small@users.noreply.github.com>
2025-09-10 09:45:06 +0200

    Initial Skeleton

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navMenu = document.querySelector('.nav-menu');
+
+    burgerMenu.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+});
